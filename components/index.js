27addcfa62508b17bdc753af9201db75c660be7c
ChenKun <13622007100m0@sina.cn>
2017-05-23 21:52:26 +0800

    陈坤修改头部bug

--- components/index.js
+++ components/index.js
@@ -3,29 +3,6 @@ import ReactDOM from 'react-dom';
 import Head from './views/head';
 import { Router, Route} from 'react-router';
 
-// class Index extends React.Component {
-//   render(){
-//     return (
-//       <div>
-//         <h1>Hello</h1>
-//       </div>
-//     );
-//   }
-// }
-
-var Index = React.createClass({
-  render(){
-    return (
-      <div>
-      <h1>Hello</h1>
-      <Head name="React">
-        React
-      </Head>
-
-      </div>
-    );
-  }
-})
 
 
 ReactDOM.render(
--- components/views/head.js
+++ components/views/head.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 
-class Head extends Component {
-  render(){
-    return (
-      <div>
-        <h1>Hello {this.props.name}</h1>
-        <form class="" action="/userinfo" method="get">
-        <Button bsStyle="primary" type='submit'>确定</Button>
-        </form>
-      </div>
-    )
-  }
-
-}
 
 module.exports = Head;
