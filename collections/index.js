e81a5b03c3c5fa8026550fdad5f8fe8b5bd8cb18
Jason Guo <jasonlearning@outlook.com>
2017-05-23 13:57:16 +0800

    Modified Mongodb file.
    Add collections.

--- collections/index.js
+++ collections/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://cnstu.top:27017/stu';
+const url = 'mongodb://127.0.0.1:27017/kdadb';
 //const url = 'mongodb://192.168.1.11:27017/stu';
 
 mongoose.connect(url);
@@ -12,10 +12,53 @@ db.once('open', function(){
 
 let Schema = mongoose.Schema;
 
-let userShema = Schema({
-  name: {type: String, default: 'admin' },
-  passwd: {type: String, default: 'admin' }
+let userSchema  = Schema({
+  account:String,
+  nickName:String,
+  password:	String,
+  gender:	Boolean,
+  age:	Number,
+  birthday:	String,
+  myCollections:	[],
+  myQuestions:	[],
+  myAnswers:	[],
+  alipayAccount:	String,
+  balance:	Number
 });
 
+let questionSchema = Schema({
+  title:String,
+  type:[],
+  content:String,
+  author:Object,
+  answerList:[],
+  charge:Boolean,
+  validTime:Number,
+  money:Number,
+  createTime:String,
+  pageviews:Number
+});
 
-module.exports.user = mongoose.model('user', userShema);
+let answerSchema = Schema({
+  question:Object,
+  Content:String,
+  answerTime:String,
+  author:Object,
+  adopt:Boolean,
+  likeNum:Number
+});
+
+let typeSchema = Schema({
+  	type:	String,
+    questionIdList:	[]
+});
+
+let billSchema = Schema({
+  consumptionRecord:[],
+  rechargeRecord:[]
+})
+module.exports.user = mongoose.model('user', userSchema);
+module.exports.question = mongoose.model('question',questionSchema);
+module.exports.answer = mongoose.model('answer',answerSchema);
+module.exports.type = mongoose.model('type',typeSchema);
+module.exports.bill = mongoose.model('bill',billSchema);
