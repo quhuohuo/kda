d0b881cfe6f746e1b3c86a82e18a428cc6a1e831
Rzn <rzn0725@outlook.com>
2017-05-23 15:12:18 +0800

    提交一次文件
    在页面中 加入 head.ejs foot.ejs
     要提交的变更：
    	修改：     routes/index.js
    	修改：     views/head.ejs
    	修改：     views/userinfo.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{user:''});
+  res.render('index',{user:'ren'});
 });
 
 router.get('/userinfo',function(req,res,next){
-  res.render('userinfo.ejs')
+  res.render('userinfo',{user:'ren'})
 })
 module.exports = router;
