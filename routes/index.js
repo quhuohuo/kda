689ba69c76a262d0edc8f85a25c6e7aebcbe3bbc
tyz <18342966003@163.com>
2017-05-26 17:46:33 +0800

    优化了注册登录界面

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var dbmodel = require('../collections');
 var bcrypt = require('bcrypt');
+var db = require('../collections');
+var salt = 10;
 
 /* GET home page. */
 
@@ -17,6 +19,55 @@ router.get('/', function(req, res, next) {
 router.get('/userinfo', function(req, res, next) {
   res.render('userinfo', {
     user: 'ren'
+  })
+})
+
+router.get('/login',function(req,res,next){
+  res.render('login',{user:'no',password:'no'});
+})
+
+router.post('/login',function(req,res,next){
+  db.user.findOne({account:req.body.account},function(err,data){
+    if(data){
+      bcrypt.compare(req.body.password,data.password,function(err,hash){
+        if(hash){
+          var addr = req.body.account;
+          res.cookie('addr',addr,{maxAge:1000*60*60*24*30,httpOnly:true});
+          req.session.user = data.nickName;
+          req.session.userid = data._id;
+          req.session.account = data.account;
+          res.redirect('/');
+        }else{
+          res.render('login',{user:'no',password:'yes'})
+        }
+      })
+    }else{
+      res.render('login',{user:'yes',password:'no'})
+    }
+  })
+})
+
+router.get('/reg',function(req,res,next){
+  res.render('reg',{user:null,cunzai: 'no'});
+})
+
+router.post('/reg',function(req,res,next){
+  db.user.findOne({account:req.body.account},function(err,data){
+    if(data){
+      res.render('reg',{user:'no',cunzai: 'yes'})
+    }else{
+      var user = new db.user({
+        account:req.body.account,
+        password:req.body.password
+      });
+
+      bcrypt.hash(req.body.password, salt,function(err,hash){
+        user.password = hash;
+        user.save(function(err,data){
+          res.render('login',{user:'no',password:'no'})
+        })
+      })
+    }
   })
 })
 
