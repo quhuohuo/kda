53c5195e2a44ab691542d29c6d1b2749dd6c2125
Jason Guo <jasonlearning@outlook.com>
2017-05-27 11:17:26 +0800

    修改排版格式,解决Promise问题
    修改排版格式,解决Promise问题

--- collections/index.js
+++ collections/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const url = 'mongodb://127.0.0.1:27017/kdadb';
-//const url = 'mongodb://192.168.1.11:27017/stu';
 
+mongoose.Promise = global.Promise;
 mongoose.connect(url);
 
 let db = mongoose.connection;
@@ -14,52 +14,152 @@ let Schema = mongoose.Schema;
 let ObjectId = Schema.Types.ObjectId;
 
 let userSchema = Schema({
-  account:{type:'String',requried:true},
-  nickName:{type:'String',default:'user'},
-  password:{type:'String',required:true},
-  gender:{type:'Boolean',default:true}, 
-  age:{type:'Number',default:0},
-  birthday:{type:'String',default:'1990-01-01'},
-  myCollections:[{type:'ObjectId',ref:'question'}],
-  myQuestions:[{type:'ObjectId',ref:'question'}],
-  myAnswers:[{type:'ObjectId',ref:'question'}],
-  alipayAccount:{type:'String',default:''},
-  balance:{type:'Number',default:100},
-  headPortrait:{type:'String',default:'./iamges.head.jpg'},
-  backgroundImage:{type:'String',default:''},
-  totalReward:{type:'Number',default:0}
+  account: {
+    type: 'String',
+    requried: true
+  },
+  nickName: {
+    type: 'String',
+    default: 'user'
+  },
+  password: {
+    type: 'String',
+    required: true
+  },
+  gender: {
+    type: 'Boolean',
+    default: true
+  },
+  age: {
+    type: 'Number',
+    default: 0
+  },
+  birthday: {
+    type: 'String',
+    default: '1990-01-01'
+  },
+  myCollections: [
+    {
+      type: 'ObjectId',
+      ref: 'question'
+    }
+  ],
+  myQuestions: [
+    {
+      type: 'ObjectId',
+      ref: 'question'
+    }
+  ],
+  myAnswers: [
+    {
+      type: 'ObjectId',
+      ref: 'question'
+    }
+  ],
+  alipayAccount: {
+    type: 'String',
+    default: ''
+  },
+  balance: {
+    type: 'Number',
+    default: 100
+  },
+  headPortrait: {
+    type: 'String',
+    default: './iamges.head.jpg'
+  },
+  backgroundImage: {
+    type: 'String',
+    default: ''
+  },
+  totalReward: {
+    type: 'Number',
+    default: 0
+  }
 });
 
 let questionSchema = Schema({
-  title:{type:'String',default:''},
-  type:[],
-  content:{type:'String',default:''},
-  author:{type:'ObjectId',ref:'user'},
-  answerList:[{type:'ObjectId',ref:'question'}],
-  charge:{type:'Boolean',default:false},
-  validTime:{type:'Number',default:0},
-  money:{type:'Number',default:0},
-  createTime:{type:'String',default:''},
-  pageviews:{type:'Number',default:0}
+  title: {
+    type: 'String',
+    default: ''
+  },
+  type: [],
+  content: {
+    type: 'String',
+    default: ''
+  },
+  author: {
+    type: 'ObjectId',
+    ref: 'user'
+  },
+  answerList: [
+    {
+      type: 'ObjectId',
+      ref: 'question'
+    }
+  ],
+  charge: {
+    type: 'Boolean',
+    default: false
+  },
+  validTime: {
+    type: 'Number',
+    default: 0
+  },
+  money: {
+    type: 'Number',
+    default: 0
+  },
+  createTime: {
+    type: 'String',
+    default: ''
+  },
+  pageviews: {
+    type: 'Number',
+    default: 0
+  }
 });
 
 let answerSchema = Schema({
-  question:{type:'ObjectId',ref:'question'},
-  Content:{type:'String',default:''},
-  answerTime:{type:'String',default:''},
-  author:{type:'ObjectId',ref:'user'},
-  adopt:{type:'Boolean',default:false},
-  likers:[]
+  question: {
+    type: 'ObjectId',
+    ref: 'question'
+  },
+  Content: {
+    type: 'String',
+    default: ''
+  },
+  answerTime: {
+    type: 'String',
+    default: ''
+  },
+  author: {
+    type: 'ObjectId',
+    ref: 'user'
+  },
+  adopt: {
+    type: 'Boolean',
+    default: false
+  },
+  likers: []
 });
 
 let typeSchema = Schema({
-  type:	{type:'String',default:''},
-  questionIdList:[{type:'ObjectId',ref:'question'}]
+  type: {
+    type: 'String',
+    default: ''
+  },
+  questionIdList: [
+    {
+      type: 'ObjectId',
+      ref: 'question'
+    }
+  ]
 });
 
 let billSchema = Schema({
-  consumptionRecord:[],
-  rechargeRecord:[]
+  consumptionRecord: [],
+  rechargeRecord: []
 })
 module.exports.user = mongoose.model('user', userSchema);
 module.exports.question = mongoose.model('question',questionSchema);
