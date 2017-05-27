3a4d8f21ca487e23287e9611b71e6e0a5165ab35
quhuohuo <qh_work@outlook.com>
2017-05-27 14:48:35 +0800

    Question page is finish
    Question page is finish

--- components/question/question.js
+++ components/question/question.js
@@ -10,36 +10,113 @@ import { deepPurple900, deepPurple300, yellowA700, grey500, green800, redA700 } 
 
 
 var Index = React.createClass({
+  getInitialState() {
+    var apt = $('.question-like').text();
+    if (apt === 'true') {
+      apt = true
+    } else {
+      apt = false
+    };
+    var data = $('.question-content').text();
+    return {
+      editorHtml: '',
+      like: false,
+      collection: false,
+      adopt: apt,
+      data: data,
+    }
+  },
   changeQuill(html) {
       this.setState( Object.assign( {}, this.state, {editorHtml:html} ) );
+      // this.setState({editorHtml: html});
+  },
+  submit() {
+    var user = $('.question-user').text();
+    var id = $('.question-id').text();
+    $.ajax({
+      type: 'post',
+      url: '/question/comment',
+      data: {Content: this.state.editorHtml, author: '5928dda0d3e92d69081124e3', answerTime: new Date(), question: '5928e40767cfd96c99735fd1'},
+      dataType: 'json',
+      success: function(data,status) {
+        // console.log(data);
+      }
+    })
+  },
+
+  likebtn() {
+    var userid = $('.question-user').text();
+    var questionid = $('.question-id').text();
+    $.ajax({
+      type: 'post',
+      url: 'question/like',
+      data: {userid: '5928dda0d3e92d69081124e3', questionid: '5928e40767cfd96c99735fd1'},
+      dataType: 'json',
+      success: function(data,status) {
+      }
+    })
+    this.setState({like: !this.state.like})
+  },
+
+  collection() {
+    var userid = $('.question-user').text();
+    var questionid = $('.question-id').text();
+    $.ajax({
+      type: 'post',
+      url: 'question/collection',
+      data: {userid: '5928dda0d3e92d69081124e3', questionid: '5928e40767cfd96c99735fd1'},
+      dataType: 'json',
+      success: function(data,status) {
+      }
+    })
+    this.setState({collection: !this.state.collection})
+  },
+
+  adopt() {
+    var userid = $('.question-user').text();
+    var questionid = $('.question-id').text();
+    $.ajax({
+      type: 'post',
+      url: 'question/adopt',
+      data: {userid: '5928dda0d3e92d69081124e3', questionid: '5928e40767cfd96c99735fd1', state:this.state.adopt},
+      dataType: 'json',
+      success: function(data,status) {
+      }
+    })
+    this.setState({adopt: !this.state.adopt})
   },
 
   render(){
+    var text = this.state.like ? '取消' : '点赞';
+    var text1 = this.state.collection ? '已收藏' : '收藏';
+    var text2 = this.state.adopt ? '已采纳' : '采纳';
     return (
       <div className='question-main'>
 
         <Jumbotron className='question-title'>
-          <h1>问题标题!</h1>
-          <p>问题内容.</p>
+          <h2>{this.state.data}</h2>
+          <p>问题内容.水电费水电费收费是否说法个人个人供热哈哈和给对方会如何如何如何收到货今天以具体要看具体客户私人将同意就后天人格和人个人个人</p>
           <p>关键字：node,react</p>
           <Media>
            <Media className='question-img'>
-              <img width={30} height={30} src="/assets/thumbnail.png" alt="Image"/>
+              <img width={30} height={30} src="./images/head.jpg" alt="Image"/>
               <span>用户名</span>
               <span>浏览量：10 </span><span> 倒计时：60</span>
 
             </Media>
           </Media>
-          <p><Button bsStyle="primary">收藏</Button></p>
+          <p className='question-btn' backgroundColor={deepPurple300}><Button bsStyle="primary" onClick={this.collection}>{text1}</Button></p>
+          <p className='question-btn' backgroundColor={deepPurple300}><Button bsStyle="success" onClick={this.likebtn}>{text}</Button></p>
           <Row className="show-grid">
             <Col lg={12} md={12} sm={12} xs={12} className='question-answer'>
               <div>
-                <h1>问题标题!</h1>
-                <Media className='question-img'>
-                <img width={30} height={30} src="/assets/thumbnail.png" alt="Image"/>
+
+                <div className='question-img'>
+                <img width={30} height={30} src="./images/head.jpg" alt="Image"/>
                 <span>用户名: </span>
                 <span>评论内容.</span>
-                </Media>
+                </div>
+                <p className='question-btn' backgroundColor={deepPurple300}><Button bsStyle="danger" onClick={this.adopt}>{text2}</Button></p>
               </div>
             </Col>
             <Col lg={12} md={12} sm={12} xs={12} >
@@ -47,7 +124,7 @@ var Index = React.createClass({
             onChange={(html) => { this.changeQuill(html) }}
             placeholder={'please input'}
             />
-            <p className="question-btn"><Button bsStyle="primary">提交</Button></p>
+            <p className="question-btn"><Button bsStyle="primary" onClick={this.submit}>提交</Button></p>
             </Col>
           </Row>
 
--- routes/question.js
+++ routes/question.js
@@ -1,10 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var dbmodel = require('../collections');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('question',{'user':''});
+  dbmodel.question.findOne({_id: '5928e40767cfd96c99735fd1'},function(err,data) {
+    console.log(data.pageviews);
+    dbmodel.question.update({_id: '5928e40767cfd96c99735fd1'},{$set:{'pageviews': data.pageviews + 1}},function() {})
+  })
+  res.render('question',{'user':'', 'title': '测试问题标题'});
 });
 
+router.post('/comment',function(req, res, next) {;
+  let quest = new dbmodel.answer(req.body);
+  quest.save(function(err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+  dbmodel.question.findOne({_id: req.body.question}, function(err,data) {
+    var title = data._id;
+    dbmodel.user.findOne({_id: req.body.author}, function(err,data1) {
+      dbmodel.user.update({_id: req.body.author},{$push:{'myAnswers': title}},function() {})
+    })
+  })
+});
+
+router.post('/collection',function(req, res, next) {
+  dbmodel.user.where('myCollections').in([req.body.questionid]).exec(function(err, data1) {
+    if(data1.length) {
+      dbmodel.question.findOne({_id: req.body.questionid}, function(err,data) {
+        var id = data._id
+        dbmodel.user.update({_id: req.body.userid},{$pop:{'myCollections': id}},function() {})
+      })
+    } else {
+      dbmodel.question.findOne({_id: req.body.questionid}, function(err,data) {
+        var id = data._id
+        dbmodel.user.update({_id: req.body.userid},{$push:{'myCollections': id}},function() {})
+      })
+    }
+  })
+});
+
+
+
+
+router.post('/like',function(req, res, next) {
+  console.log(req.body);
+  dbmodel.answer.findOne({question: req.body.questionid}, function(err,data) {
+    dbmodel.answer.where('likers').in([req.body.userid]).exec(function(err,data1){
+      console.log(data1);
+      if(data1.length) {
+        dbmodel.answer.update({question: req.body.questionid}, {$pop: {'likers': req.body.userid}}, function() {})
+      } else {
+        dbmodel.answer.update({question: req.body.questionid}, {$push: {'likers': req.body.userid}}, function() {})
+      }
+    })
+  })
+});
+
+router.post('/adopt', function(req, res, next) {
+  console.log(req.body);
+  dbmodel.answer.update({question: req.body.questionid}, {$set: {'adopt': req.body.state}}, function() {})
+})
 
 module.exports = router;
