62af3ff451d1c7c7a89ccad9d095fcd73a8a60c3
quhuohuo <qh_work@outlook.com>
2017-06-07 08:55:28 +0800

    Update api file
    Add saveQuestion getQuestionById saveAnswer

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var dbmodel = require('../collections');
 var redis = require('../redis');
 
+// 所有问题详情
 router.route('/')
       .get(function(req, res) {
         dbmodel.question.find({},function(err, data) {
@@ -15,6 +16,7 @@ router.route('/')
         })
       });
 
+// 首页问题列表
 router.route('/redis/question')
       .get(function(req, res) {
         redis.getLastQuestion(function(data) {
@@ -22,6 +24,7 @@ router.route('/redis/question')
         })
       });
 
+// 首页达人榜
 router.route('/redis/talent')
       .get(function(req, res) {
         redis.getTopUser(function(data) {
@@ -29,6 +32,7 @@ router.route('/redis/talent')
         })
       });
 
+// 首页赏金榜
 router.route('/redis/money')
       .get(function(req, res) {
         redis.getTopRewardQuestion(function(data) {
@@ -36,6 +40,7 @@ router.route('/redis/money')
         })
       });
 
+// 首页热搜榜
 router.route('/redis/hot')
       .get(function(req, res) {
         redis.getTopQuestion(function(data) {
@@ -43,6 +48,7 @@ router.route('/redis/hot')
         })
       });
 
+// 首页标签类型
 router.route('/redis/alltype')
       .get(function(req, res) {
         redis.getAllTypes(function(data) {
@@ -50,7 +56,7 @@ router.route('/redis/alltype')
         })
       });
 
-
+// 问题展示页面问题详情
 router.route('/question/:id')
       .get(function(req, res) {
         dbmodel.question.findOne({_id: req.params.id})
@@ -70,6 +76,7 @@ router.route('/question/:id')
 
       })
 
+// 问题展示页面评论
 router.route('/question/comment')
       .post(function(req, res) {
         let quest = new dbmodel.answer(req.body);
@@ -95,6 +102,7 @@ router.route('/question/comment')
                           })
       })
 
+// 问题展示页面点赞
 router.route('/question/like/:id/:ip')
       .post(function(req, res) {
 
@@ -109,6 +117,7 @@ router.route('/question/like/:id/:ip')
         })
       })
 
+// 问题展示页面收藏
 router.route('/question/collection/:id/:ip')
       .post(function(req, res) {
         dbmodel.user.where('myCollections').in([req.params.id]).exec(function(err, data1) {
@@ -126,6 +135,7 @@ router.route('/question/collection/:id/:ip')
         })
       })
 
+// 问题展示页面采纳
 router.route('/question/adopt/:id/:ip')
       .post(function(req, res) {
         dbmodel.answer.update({question: req.body.questionid}, {$set: {'adopt': !req.body.state}}, function() {
@@ -135,6 +145,81 @@ router.route('/question/adopt/:id/:ip')
               dbmodel.user.update({_id: req.body.user}, {$set: {'balance': data2}}, function() {})
             })
           })
+        })
+      })
+
+// 问题id获得问题详情
+router.route('/questionid/:id')
+      .get(function(req, res) {
+        redis.getQuestionById(function(data) {
+          res.end(JSON.stringify(data));
+        })
+      })
+
+// 保存新问题
+router.route('/savequestion')
+      .put(function(req, res) {
+        if(!req.session.user){
+          res.json({state:300});
+          return;
+        }
+        var id = req.session.user._id;
+        var question = {
+          title:req.body.title,
+          type:req.body.type,
+          content:req.body.editorHtml,
+          author:id,
+          answerList:[],
+          charge:false,
+          validTime:0,
+          money:0,
+          createTime:moment().format('YYYY-MM-DD HH:mm:ss'),
+          pageviews:0
+        };
+        if(req.body.pay=='true'){
+          question.charge = true;
+          question.validTime = Number(req.body.time)*3600+Math.floor(Number(moment().format('x'))/1000);
+          question.money = req.body.count;
+        }
+        if(typeof req.body.type=='string'){
+          question.type = [req.body.type];
+        }
+        redis.saveNewQuestion(question, (err, user)=>{
+          if(err){
+            console.error(err);
+            res.json({state:400});
+          }else{
+            req.session.user.balance = user.balance - question.money;
+            res.json({state:200});
+          }
+        });
+
+      })
+
+// 保存新的评论
+router.route('/saveNewAnswer')
+      .put(function(req, res) {
+        if(!req.session.user){
+          res.json({state:300});
+          return;
+        }
+        var answer = {
+          uid: req.session.user._id,
+          qid: req.body.questionid,
+          answer: {
+            question: req.body.questionid,
+            author: req.body.user._id,
+            Content: req.body.content,
+            answerTime: moment().format('YYYY-MM-DD HH:mm:ss')
+          }
+        };
+        redis.saveNewAnswer(answer, (err, data)=>{
+          if(err){
+            console.error(err);
+            res.json({state:400});
+          } else {
+            res.json({state:200});
+          }
         })
       })
 module.exports = router;
